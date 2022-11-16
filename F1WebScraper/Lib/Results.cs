71b00c85e7427c98625f43a3e3a204ab858c0877
Radosław Krowicki <rkkrowicki@gmail.com>
2022-11-16 23:38:09 +0100

    Added web scraping.

--- F1WebScraper/Lib/Results.cs
+++ F1WebScraper/Lib/Results.cs
@@ -3,13 +3,22 @@ using HtmlAgilityPack;
 
 namespace F1WebScraper.Lib;
 
-public class Results
+public static class Results
 {
-    public List<string> GetResults(int year, int raceNumber)
+    public static List<string> GetResults(int year, int raceNumber)
     {
         var noRace = 1124 + raceNumber;
+        var results = new List<string>();
+        var url = $"https://www.formula1.com/en/results.html/{year}/races/{noRace}/race-result.html";
         HtmlWeb web = new HtmlWeb();
-        HtmlDocument doc = web.Load($@"https://www.formula1.com/en/results.html/{year}/races/{noRace}/race-result.html");
-        return new List<string>();
+        HtmlDocument doc = web.Load(url);
+        var tableXPath = "//tr";
+        var resultsNodes = doc.DocumentNode.SelectNodes(tableXPath).Skip(1).Select(x => x.ChildNodes[5].InnerHtml);
+        //doc.DocumentNode.SelectNodes("//tr")[1].ChildNodes[3]
+        foreach (var resultsNode in resultsNodes)
+        {
+            results.Add(resultsNode);
+        }
+        return results;
     }
 }
--- F1WebScraper/Program.cs
+++ F1WebScraper/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices.ComTypes;
+using F1WebScraper.Lib;
+
+var res = Results.GetResults(2022, 1);
 Console.WriteLine("Hello, World!");
